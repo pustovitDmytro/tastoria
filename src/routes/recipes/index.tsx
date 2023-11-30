da1064b4848994208aab119ec121ad7d721ee15e
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2023-11-30 02:22:38 +0200

    Fix: drop redunant checks

--- src/routes/recipes/index.tsx
+++ src/routes/recipes/index.tsx
@@ -4,25 +4,14 @@ import { routeLoader$ } from '@builder.io/qwik-city';
 import type { Receipt } from '~/components/RecipesList/RecipesList';
 import List from '~/components/RecipesList/RecipesList';
 import firebase from '~/firebase';
-import { sessionContext } from '~/stores/session';
 
 export const useRecipesDetails = routeLoader$(async () => {
-    try {
-        const res = await firebase.downloadData();
+    const res = await firebase.downloadData();
 
-        return (res.recipes as Receipt[]);
-    } catch (error) {
-        console.error(error);
-
-        return [];
-    }
+    return (res.recipes as Receipt[]);
 });
 
 export default component$(() => {
-    const session = useContext(sessionContext);
-
-    if (!session.user) return (<> User not found</>);
-
     const result = useRecipesDetails();
 
     return (
